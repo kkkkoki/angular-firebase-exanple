874f392d7741f7e3aa552fe6931fe20bac9fe6fe
unknown <koki_inoue45@icloud.com>
2020-06-11 14:34:31 +0900

    feat(firebase): firebaseの追加設定

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,14 +7,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAnPeXs_cMwxdpX24VywJNTrxUVRTVYxxc",
-    authDomain: "gitpet-ff96f.firebaseapp.com",
-    databaseURL: "https://gitpet-ff96f.firebaseio.com",
-    projectId: "gitpet-ff96f",
-    storageBucket: "gitpet-ff96f.appspot.com",
-    messagingSenderId: "360365896859",
-    appId: "1:360365896859:web:af1e67518392d59da7023e",
-    measurementId: "G-F6PE6LNZZH",
+    apiKey: 'AIzaSyAnPeXs_cMwxdpX24VywJNTrxUVRTVYxxc',
+    authDomain: 'gitpet-ff96f.firebaseapp.com',
+    databaseURL: 'https://gitpet-ff96f.firebaseio.com',
+    projectId: 'gitpet-ff96f',
+    storageBucket: 'gitpet-ff96f.appspot.com',
+    messagingSenderId: '360365896859',
+    appId: '1:360365896859:web:af1e67518392d59da7023e',
+    measurementId: 'G-F6PE6LNZZH',
   },
 };
 
