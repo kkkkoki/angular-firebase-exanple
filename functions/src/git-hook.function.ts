3518d84c880501c27dfd505f9638563d4c2f0033
unknown <koki_inoue45@icloud.com>
2020-06-12 17:08:42 +0900

    feat(gitHook): GitHubのWebhook追加

---
+++ functions/src/git-hook.function.ts
@@ -0,0 +1,7 @@
+import * as functions from 'firebase-functions';
+
+export const gitHook = functions.https.onRequest((request, response) => {
+  console.log(request.body);
+
+  response.send('success');
+});
