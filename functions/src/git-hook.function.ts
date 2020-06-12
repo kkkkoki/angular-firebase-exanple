1664d6e40fa662defa0ea9f31b13d1c2584f278e
unknown <koki_inoue45@icloud.com>
2020-06-12 17:24:50 +0900

    ini

--- functions/src/git-hook.function.ts
+++ functions/src/git-hook.function.ts
@@ -1,7 +1,6 @@
 import * as functions from 'firebase-functions';
 
 export const gitHook = functions.region('asia-northeast1').https.onRequest((request, response) => {
-  console.log(request.body);
-
+  console.log(request.body.sender.id);
   response.send('success');
 });
